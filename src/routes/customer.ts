5c3e00cf663c0f3cfa3c890117ac714978f4feb9
Rajbeer Singh <rajbeersingh@Rajbeers-MacBook-Air.local>
2024-04-29 12:49:13 +0530

    delete

--- src/routes/customer.ts
+++ src/routes/customer.ts
@@ -2,4 +2,4 @@ import { Router } from "express";
 import { add, deletee, getall, getbyid, update } from "../controllers/customer";  
 
 export const customerRoutes = Router();
-customerRoutes.get('/', getall).get("/:id",getbyid).post('/',add).put('/:id',update).delete(':/id',deletee);
+customerRoutes.get('/', getall).get("/:id",getbyid).post('/',add).put('/:id',update).delete('/:id',deletee);
